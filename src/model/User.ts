5f89197e172ee57982bc6b3ea24bc8113bbf1d81
Rahul Yadav <rahulyadav989290@gmail.com>
2024-11-05 11:41:49 +0530

    creating the schema folder and creating file in it

--- src/model/User.ts
+++ src/model/User.ts
@@ -17,3 +17,57 @@ const MessageSchema: Schema<Message> = new Schema({
     default: Date.now,
   },
 });
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptedMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "verify code is required is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "verify code is Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptedMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
